003f277208806817326d77902c4424bf6b61ef39
sevincesedova <161349938+sevincesedova@users.noreply.github.com>
2024-03-27 14:03:01 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,51 @@
+// task1
+// let eded = +prompt("ededi daxil edin")
+// for (let i=2; i<=eded**0.5; i++){
+//     if(eded%i===0){
+//         console.log(`${eded} murekkebdir`);}
+//     else {
+//         console.log(`${eded} sadedir`);
+//     }
+// }  
+    
+
+// task2
+// let eded = +prompt(" ededi daxil edin");
+//   let s= 0;
+//  for (let i = 1; i < eded; i++) {
+//       if (eded % i === 0) {
+//           s += i;
+//       }
+//  }
+//  if (s === eded) {
+//      console.log(`${eded} mukemmeldir`);
+//  } 
+//  else {
+//      console.log(`${eded} mukemmeldir`);
+//   }
+ 
+
+// task5
+// let eded1 = +prompt("1-ci ədədi daxil edin");
+// let eded2 = +prompt("2-ci ədədi daxil edin");
+// for(let eded=eded1; eded<= eded2;eded++){
+//     let data = true;
+//     if(eded.toString().endsWith('7')){
+//         console.log(eded);
+//     }
+// }
+
+// task4
+// let eded1 = prompt("ededi daxil edin")
+// let eded2 = prompt("ededi daxil edin")
+// for (let i=2; i<=eded1**0.5; i++){
+//         if(eded1%i!==0){
+//             console.log(`${eded1} sadedir`);}
+//         else if(eded2%i!==0)
+//         {
+//             console.log(`${eded2} sadedir`);}
+//         else{
+//             console.log("hec biri")
+//         }
+//     }  
+
