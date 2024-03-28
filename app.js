371e568c3ac2322f24d85b5116e773e1ebbc7911
sevincesedova <161349938+sevincesedova@users.noreply.github.com>
2024-03-28 18:12:26 +0400

    update

--- app.js
+++ app.js
@@ -1,84 +1,83 @@
+// task1
+// let eded = +prompt("ededi daxil edin")
+// result=true
+// for (let i=2; i<=eded/2; i++){
+//     if(eded%i===0){
+//           result=false
+//           break
+        
+//         }
+// } 
+// if(result){
+//     alert(`${eded}ededi sadedir `)
+// }
+// else{
+//     alert(`${eded}ededi sade deyil `)
+// } 
+    
+
+// task2
+// let eded = +prompt(" ededi daxil edin");
+//   let s= 0;
+//  for (let i = 1; i < eded; i++) {
+//       if (eded % i === 0) {
+//           s += i;
+//       }
+//  }
+//  if (s === eded) {
+//      alert(`${eded} mukemmeldir`);
+//  } 
+//  else {
+//      alert(`${eded} mukemmel deyil`);
+//   }
+ 
+// task3
+
+// let eded1 = +prompt("1ci ededi daxil edin: ");
+// let eded2 = +prompt("2ci ededi daxil edin: ");
+// for (let j = eded1; j <= eded2; j++) {
+//   for (let i = 1; i <= j; i++) {
+//     if (j % i === 0 && i !== 1) {
+//       if (j > i) {
+//         break;
+//       }
+//       if (j === i) {
+//         alert(i);
+//       }
+//     }
+//   }
+// }
+
+// task5
+// let eded1 = +prompt("1-ci ədədi daxil edin");
+// let eded2 = +prompt("2-ci ədədi daxil edin");
+// for(let eded=eded1; eded<= eded2;eded++){
+//     let data = true;
+//     if(eded.toString().endsWith('7')){
+//         alert(eded);
+//     }
+// }
+
+// task4
+// let eded1 = prompt("ededi daxil edin")
+// let eded2 = prompt("ededi daxil edin")
+// for (let i=2; i<=eded1/2; i++){
+//         if(eded1%i!==0){
+//             alert(`${eded1} sadedir`);}
+//         else if(eded2%i!==0)
+//         {
+//             alert(`${eded2} sadedir`);}
+//         else{
+//             alert("hec biri")
+//         }
+//     }  
+
+    
 
  
 
 
 
 
-//  TASK
+ 
    
-// task1
-// let numbers=[1,3,5,7,9,2,8,6,4,0]
-// console.log(numbers)
-// const numbers_sort=numbers.sort((a,b)=>b-a)
-// console.log(numbers[0])
-
-// task3
-// let data=["salam","dunyali","javascript","proqramlasdirma","kod","frontend","backend"]
-// console.log(data)
-// const data_filter=data.filter((data) => data.length ===7)
-// console.log(data_filter)
-
-// task2
-let numbers=[1,5,7,9,2,8,6,4,13,41,44]
-function sade(num){
-    for(i=2; i<=num/2; i++){
-        if(num%i===0){
-            return false
-        }
-    }
-      return true
-}
-console.log(numbers.filter(sade));
-
-// task4
-// let kilo=+prompt("kilonuzu daxil edin:")
-// let boy=+prompt("boyunuzu m ile daxil edin:")
-// let kutle_indeksi=kilo/boy**2
-
-// if(kutle_indeksi<18){
-//     alert("ideal çekiden asagi")
-// }
-// else if(kutle_indeksi>=18 && kutle_indeksi<25){
-//    alert("ideal çeki")
-// }
-// else if(kutle_indeksi>=25 && kutle_indeksi<30){
-//     alert("ideal çekiden yuxari")
-// }
-// else if(kutle_indeksi>=30 && kutle_indeksi<40){
-//     alert("artiq ceki")
-// }
-// else {
-//     alert("piylenme")
-// }
-
-// task5
-// let bal=prompt("balinizi daxil edin")
-// if(bal<=100 && bal>90){
-//     alert("ela")
-// }
-// else if(bal<=90 && bal>75){
-//     alert("yaxsi")
-// }
-// else if(bal<=75 && bal>55){
-//     alert("orta")
-// }
-// else if(bal<=55 && bal>35){
-//     alert("pis")
-// }
-// else if(bal<=35){
-//     alert("ela")
-// }
-// else {
-//     alert("balinizi duzgun daxil edin")
-// }
-
-// task6
-// let a= + prompt("ucbucagin birinci terefini daxil edin:")
-// let b= +prompt("ucbucagin birinci terefini daxil edin:")
-// function hipotenuz(a,b) {
-//     c=a**2+b**2;
-//     k=Math.sqrt(c);
-//   return k; 
-// }
-
-// alert(hipotenuz(a,b))
